b6a528e9a6cae8d08a53fa6ffeb9eff9ed2568ce
System D <rudradev93@gmail.com>
2024-12-02 20:38:00 +0530

    Create test.cpp

---
+++ 23/test.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<iostream>>
+//#include<bits/stdc++.h>
+using namespace std;
+void printPat(int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n;
+	cin>>n;
+    printPat(n);
+    cout<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+void printPat(int n)
+{
+//Your code here
+
+// for(int i = n; i<0; i--)
+// {
+//     for(int j = )
+//     //cout<<i;
+// }
+
+    //Your code here
+    for(int i = n; i > 0; i--)
+    {
+        for(int j = n; j > 0; j--)
+        {
+            for(int k = 0; k < i; k++)
+            {
+                cout << j << " ";
+            }
+        }
+        cout << "$";
+    }
+
+
+}
